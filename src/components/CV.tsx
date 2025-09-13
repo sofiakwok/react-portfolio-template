f2d040fdf650b19d1fc03e98269b48a3c2fff199
sofiakwok <zazakwok@gmail.com>
2025-09-12 17:08:13 -0700

    adding cv

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -19,11 +19,6 @@ function CV() {
             <a href="https://www.linkedin.com/in/sofiakwok/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Sofia Kwok</h1>
-          <p>Hello! I am an engineer interested in space, robots, and space robots. 
-            I currently work at Starfish Space, an in-orbit satellite servicing startup in Seattle. </p>
-          <p>I have a Master of Science in Robotics from Carnegie Mellon University and a Bachelor of Science in Mechanical Engineering from the California Institute of Technology. 
-            My past experience includes building a bipedal robot, designing a percussive drill for lunar regolith, and making robot arms that can solve jigsaw puzzles and play Bananagrams.
-            For more details, refer to my CV.</p>
           <div className="mobile_social_icons">
             <a href="https://github.com/sofiakwok" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/sofiakwok/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
